e37f9b1869c2c309534d9cfdc8da3030a463e6e7
mustapha <elmustaphaes.salmi@gmail.com>
2024-09-18 13:13:57 +0200

    fix font familly cairo

--- components/ArabicText/ArabicText.js
+++ components/ArabicText/ArabicText.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
-import { useFonts } from "expo-font";
-//import AppLoading from "expo-app-loading"; // This helps to wait while fonts are loading
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading"; 
 
 const ArabicText = ({ text, fweight, fsize }) => {
-  // Load fonts
+  const [dataLoaded, setDataLoaded] = useState(false);
 
+  const fetchFont = () => {
+    return Font.loadAsync({
+      CairoRegular: require("../../assets/fonts/static/Cairo-Regular.ttf"),
+    });
+  };
 
-  /*if (!fontsLoaded) {
-    return <AppLoading />; // Show a loader until fonts are loaded
-  }*/
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFont}
+        onFinish={() => setDataLoaded(true)}
+        onError={(error) => console.warn(error)} // Optional: to catch errors while loading fonts
+      />
+    );
+  }
 
   return (
-      <Text style={[styles.base, { fontSize: fsize || 16, fontWeight: fweight || "400" }]}>
-        {text}
-      </Text>
+    <Text
+      style={[
+        styles.base,
+        { fontSize: fsize || 16, fontWeight: fweight || "400" },
+      ]}
+    >
+      {text}
+    </Text>
   );
 };
 
 const styles = StyleSheet.create({
   base: {
     fontFamily: "CairoRegular", // The font you loaded
-    fontWeight: "400", // Default weight, you can change it dynamically
-    fontSize: 16,
+    fontWeight: "400", // Default weight, adjustable
+    fontSize: 16, // Default size, adjustable
   },
 });
 
