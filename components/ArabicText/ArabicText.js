d853da0f5b352f5f2042431de9fc8355abfc4c0e
mustapha <elmustaphaes.salmi@gmail.com>
2024-09-27 16:50:32 +0200

    use in load fonts expo-splash-screen

--- components/ArabicText/ArabicText.js
+++ components/ArabicText/ArabicText.js
@@ -1,25 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import * as Font from "expo-font";
-import AppLoading from "expo-app-loading"; 
+import * as SplashScreen from "expo-splash-screen";
 
 const ArabicText = ({ text, fweight, fsize }) => {
   const [dataLoaded, setDataLoaded] = useState(false);
 
-  const fetchFont = () => {
-    return Font.loadAsync({
+  const fetchFont = async () => {
+    await Font.loadAsync({
       CairoRegular: require("../../assets/fonts/static/Cairo-Regular.ttf"),
     });
   };
 
+  useEffect(() => {
+    const loadResources = async () => {
+      try {
+        await fetchFont();
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setDataLoaded(true);
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    loadResources();
+  }, []);
+
   if (!dataLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFont}
-        onFinish={() => setDataLoaded(true)}
-        onError={(error) => console.warn(error)} // Optional: to catch errors while loading fonts
-      />
-    );
+    return null; // Optionally, you could return a loading spinner here
   }
 
   return (
